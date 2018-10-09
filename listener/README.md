8e0522804a53aa5eb30fde1720b2bc15572ebc93
Carlos Redondo <carlos.red@utexas.edu>
2018-10-08 21:13:22 -0500

    Added a listener docker container

---
+++ listener/README.md
@@ -0,0 +1,15 @@
+Recovers the output files from a greyfish server.
+
+
+## Installation
+
+```bash
+
+# Modify the docker volume to the host's directory
+# Change the basic greyfish URL and key (these values can be altered by requests)
+vi docker-compose.yml
+
+# Start the container
+docker-compose up -d
+
+```
---
+++ listener/listener.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+"""
+BASICS
+
+After receiving a ping request, calls a greyfish system for the output files
+"""
+
+import os, sys, shutil
+from flask import Flask
+import requests
+import tarfile
+import uuid
+
+
+app = Flask(__name__)
+DOWNLOAD_PATH = os.environ['output_data_path']
+CURDIR = dir_path = os.path.dirname(os.path.realpath(__file__)) # Current director
+greyfish_url = os.environ['greyfish_url']
+greyfish_key = os.environ['greyfish_key']
+
+
+# Provided a tar file path, it will untar all its contents into a second directory
+# It will also delete the tar file
+# tpath, destination (str): Full paths
+def unpack_tar(tpath, destination):
+
+    tar = tarfile.open(tpath)
+    tar.extractall(destination)
+    tar.close()
+    os.remove(tpath)
+
+
+
+@app.route("/listener/api/users/output_data", methods=['POST'])
+def output_data():
+
+    # Gets the entire output 
+    INFO = request.get_json()
+
+    """
+    {
+    # REQUIRED
+    Job_ID:"str"
+    User: "str"
+    OUTPUT_DIRS : [...] # Empty if nothing, each directory must be done following greyfish convention
+    EXTRA_FILES :[{path:'',filename;''}, ...] # Empty if nothing, each path must be written with greyfish syntax 
+
+    # OPTIONAL
+    greyfish_url = "new url"
+    greyfish_key = "new password"
+    
+    }
+    """
+
+    try:
+        Job_ID = INFO['Job_ID']
+        User_ID = INFO['User']
+        EXTRA_DIRS = INFO['OUTPUT_DIRS']
+        EXTRA_FILES = INFO['OUTPUT_FILES']
+    except:
+        return "INVALID, mandatory fields not provided"
+
+    # Deals with the optional parameters for greyfish
+    if 'greyfish_url' in INFO:
+        greyfish_url = INFO['greyfish_url'].replace("http://", '')
+    if 'greyfish_key' in INFO:
+        greyfish_key = INFO['greyfish_key']
+
+    URL="http://"+greyfish_url+":2000/grey/grey_dir/"+greyfish_key+"/"+User_ID+"/"+Job_ID
+
+    # Downloads the required data
+    fnam = "/listener/tmp/"+str(uuid.uuid4())+".tar.gz"
+
+    r = requests.get(URL, stream=True)
+    with open(fnam, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+    JOB_FOLDER = DOWNLOAD_PATH+"/"+User_ID+"/"+Job_ID
+
+    try:
+        # Deletes any folders with outputs corresponding to the sam Job_ID
+        if os.path.exists(JOB_FOLDER):
+            shutil.rmtree(JOB_FOLDER)
+
+        os.makedirs(JOB_FOLDER)
+        unpack_tar(fnam, JOB_FOLDER)
+
+    except:
+        return "Could not read output data"
+
+
+    # Downloads all the extra directories within the same folder but one folder down
+    # Any files already present with the same name will be rewritten
+    for D2 in EXTRA_DIRS:
+        URL = "http://"+greyfish_url+":2000/grey/grey_dir/"+greyfish_key+"/"+User_ID+"/"+D2
+        fnam = "/listener/tmp/"+str(uuid.uuid4())+".tar.gz"
+        r = requests.get(URL, stream=True)
+        with open(fnam, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+        unpack_tar(fnam, JOB_FOLDER)
+
+
+    # Downloads all the extra files
+    # Any files already present with the same name will be rewritten
+    for F2 in EXTRA_FILES:
+        URL = "http://"+greyfish_url+":2000/grey/grey/"+greyfish_key+"/"+User_ID+"/"+F2['filename']+"/"+D2['path']
+        r = requests.get(URL, stream=True)
+        with open(JOB_FOLDER+'/'+F2['filename'], 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+
+    return "Data received by IPT"
+
+
+
+if __name__ == '__main__':
+    app.run()
