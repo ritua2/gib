2c6577fdc40928268b6c44320df315ec932cc362
Carlos Redondo <carlos.red@utexas.edu>
2018-10-22 12:02:56 -0500

    Added redirect and attach new instances APIs to main node

--- conductor/traffic.go
+++ conductor/traffic.go
@@ -1,15 +1,14 @@
 /*
 BASICS
-
 Redirects a user, doing all the necessary actions
 Checks that a user is valid after login into wetty
-
 */
 
 
 package main
 
 import (
+    "bytes"
     "fmt"
     "github.com/gorilla/mux"
     "github.com/go-redis/redis"
@@ -52,6 +51,14 @@ var r_redirect_cache *redis.Client = redis.NewClient(&redis.Options{
     })
 
 
+// Stores user information before identifying with wetty
+var r_before_id *redis.Client = redis.NewClient(&redis.Options{
+    Addr: "0.0.0.0:6379",
+    Password: redauth,
+    DB:2,
+    })
+
+
 func main(){
 
     r := mux.NewRouter()
@@ -59,6 +66,8 @@ func main(){
     r.HandleFunc("/api/scripts/startup", Startup_supplier).Methods("GET")
     r.HandleFunc("/api/active", Checker).Methods("GET")
     r.HandleFunc("/api/assign/users/{user_id}", Assigner).Methods("POST")
+    r.HandleFunc("/api/redirect/users/{user_id}/{target_ip}", Redirect).Methods("GET")
+    r.HandleFunc("/api/instance/attachme", Attachme).Methods("POST")
     http.Handle("/", r)
 
 
@@ -92,43 +101,112 @@ func Assigner(w http.ResponseWriter, r *http.Request){
 
     if err != nil {
         fmt.Fprintf(w, "POST parameters could not be parsed")
-    }
-
-    key := ppr.Key
-
-    all_instances_occupied := true
-
-    //reqip := ip_only(r.RemoteAddr)
-
-    if valid_adm_passwd(key){
-        instances := redkeys(r_occupied)
-
-        for _, instance := range instances{
-                // Gets the instance available
-                ava, _ := r_occupied.HGet(instance, "Available").Result()
-
-                // Ignores instances currently inc cache
-                if stringInSlice(instance, redkeys(r_redirect_cache)){
-                   continue
-                }
-
-                if ava=="Yes"{
-                    // Sets the instance as occupied, server now has 20 s to redirect user
-                    r_redirect_cache.Set(instance, UID, 20*time.Second)
-                    fmt.Fprintf(w, "User assigned to: %s", instance)
-                    all_instances_occupied = false
-                    break
-                }
-            }
-        if all_instances_occupied{
-            fmt.Fprintf(w, "CANNOT assign user, all instances are occupied")
-        }
-
     } else {
-        fmt.Fprintf(w, "INVALID key")
+
+        key := ppr.Key
+
+        all_instances_occupied := true
+
+        //reqip := ip_only(r.RemoteAddr)
+
+        if valid_adm_passwd(key){
+            instances := redkeys(r_occupied)
+
+            for _, instance := range instances{
+                    // Gets the instance available
+                    ava, _ := r_occupied.HGet(instance, "Available").Result()
+
+                    // Ignores instances currently inc cache
+                    if stringInSlice(instance, redkeys(r_redirect_cache)){
+                       continue
+                    }
+
+                    if ava=="Yes"{
+                        // Sets the instance as occupied, server now has 20 s to redirect user
+                        r_redirect_cache.Set(instance, UID, 20*time.Second)
+                        fmt.Fprintf(w, "User assigned to: %s", instance)
+                        all_instances_occupied = false
+                        break
+                    }
+                }
+            if all_instances_occupied{
+                fmt.Fprintf(w, "INVALID: cannot assign user, all instances are occupied")
+            }
+
+        } else {
+            fmt.Fprintf(w, "INVALID key")
+        }
     }
 
 }
+
+
+// Redirects a user after an available IP has been provided to him
+func Redirect (w http.ResponseWriter, r *http.Request){
+
+    UID := mux.Vars(r)["user_id"]
+    TIP := mux.Vars(r)["target_ip"]
+
+    // Finds if the user is located at any instance
+    expected_user, err := r_redirect_cache.Get(TIP).Result()
+    _, e2 := r_before_id.Get(TIP).Result()
+    if (err == redis.Nil) || (e2 != redis.Nil) {
+        fmt.Fprintf(w, "INVALID: %s has already been assigned to another user", TIP)
+    } else {
+
+        if UID == expected_user {
+            // Adds http:// to the user name
+            var b  bytes.Buffer
+            b.WriteString("http://")
+            b.WriteString(TIP)
+
+            http.Redirect(w, r, b.String(), 301)
+
+        } else {
+            fmt.Fprintf(w, "INVALID, %s is not the expected user", UID)
+        }
+    }
+}
+
+
+// Adds the caller IP as a wetty instance
+func Attachme (w http.ResponseWriter, r *http.Request){
+    var ppr Provided_Parameters
+
+    err := json.NewDecoder(r.Body).Decode(&ppr)
+
+    if err != nil {
+        fmt.Fprintf(w, "POST parameters could not be parsed")
+    } else {
+
+        key := ppr.Key
+        // Each IP has to identify itself
+        sender_ID := ppr.Sender
+        reqip := ip_only(r.RemoteAddr)
+
+        if valid_adm_passwd(key){
+
+            // All IPs are stored in Redis with the following data:
+            // Available, id, current_user, whoami_count, address
+            var new_instance = make(map[string]interface{})
+            new_instance["Available"] = "Yes"
+            new_instance["id"] = sender_ID
+            new_instance["current_user"] = "Empty"
+            new_instance["whoami_count"] = "0"
+            new_instance["address"] = reqip
+
+            _, e2 := r_occupied.HMSet(reqip, new_instance).Result()
+
+            if e2 != nil {
+                fmt.Fprintf(w, "Server error, Redis is not attached")
+            }
+            fmt.Fprintf(w, "Instance correctly attached")
+        } else {
+            fmt.Fprintf(w, "INVALID key")
+        }
+    }
+}
+
 
 
 // Checks if an administrative credential is valid
