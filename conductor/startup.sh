73e7302ec4d83937545f292ef8d1b6d60d02e3fb
Carlos Redondo <carlos.red@utexas.edu>
2018-10-26 15:08:53 -0500

    Added startup script

--- conductor/startup.sh
+++ conductor/startup.sh
@@ -2,6 +2,12 @@
 
 
 # Gets the username
+export USER=$(curl http://$MAIN_NODE:5000/api/instance/whoami)
 
 
-# Changes the terminal prompt to /PROJECT/USERNAME/:$ 
+# Gets the project
+export PROJECT=$(curl http://$MAIN_NODE:5000/api/project/name)
+
+# Changes the terminal prompt to [/PROJECT/USERNAME: ~] $ 
+
+PS1="$USER@$PROJECT: $PWD \$ " 
--- new-wetty/user_profile.sh
+++ new-wetty/user_profile.sh
@@ -3,3 +3,8 @@ source /home/ipt/.bashrc
 
 
 # Add startup script to recover user data from greyfish
+curl -O http://$MAIN_NODE:5000/api/scripts/startup
+
+source startup
+
+rm startup
