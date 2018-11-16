141591e77186cfe827e71009046cc371d1fa3261
Carlos Redondo <carlos.red@utexas.edu>
2018-11-16 15:03:38 -0600

    Fixed issues with multiple ports, ports used are now 7000-7010

--- new-wetty/main_daemon.sh
+++ new-wetty/main_daemon.sh
@@ -15,12 +15,10 @@ machine_ip=$(curl "http://$conductor:5000/api/instance/whatsmyip")
 
 # Listens to available ports in the current machine, to check which one has the current API
 current_port="NA"
-for port in $(seq 80 90);
+for port in $(seq 7000 7010);
 do
-    echo $port
-
     # Stops when it finds itself
-    NK=$(curl --max-time 10 "$machine_ip:$port")
+    NK=$(curl --max-time 10 "$machine_ip:$port") || true
 
     if [ "$NK" = "$NEW_UUID" ]; then
         current_port="$port"
@@ -41,7 +39,7 @@ fi
 
 
 # Calls the server to specify that a new instance is being created
-curl -X POST -H "Content-Type: application/json" -d '{"key":"swisspeakmontblanc", "sender":"'$NEW_UUID'", "port":"'$current_port'"}' \
+curl -X POST -H "Content-Type: application/json" -d '{"key":"'$orchestra_key'", "sender":"'$NEW_UUID'", "port":"'$current_port'"}' \
     http://$conductor:5000/api/instance/attachme
 
 
