1cda23afe118d4bb56cdd3bc9036146779f160bc
Carlos Redondo <carlos.red@utexas.edu>
2019-01-08 09:52:24 -0600

    Bug fixes

--- slurm-test/setup.sh
+++ slurm-test/setup.sh
@@ -5,6 +5,9 @@ cat docker-compose.yml > docker-centos7-slurm/docker-compose.yml
 cat Dockerfile > docker-centos7-slurm/Dockerfile
 cat docker-entrypoint.sh > docker-centos7-slurm/docker-entrypoint.sh
 
+mv json_parser.py docker-centos7-slurm/json_parser.py
+mv selector.py docker-centos7-slurm/selector.py
+
 cd docker-centos7-slurm
 
 # Needed environmental variables
@@ -12,6 +15,8 @@ printf "Enter Conductor IP (without http://): "
 read CONDUCTOR_IP
 printf "Enter conductor password: "
 read CONDUCTOR_PASSWORD
+printf "Enter output data path: "
+read ODP
 
-CONDUCTOR_IP=$CONDUCTOR_IP  CONDUCTOR_PASSWORD=$CONDUCTOR_PASSWORD docker-compose up -d --build
+CONDUCTOR_IP="$CONDUCTOR_IP"  CONDUCTOR_PASSWORD="$CONDUCTOR_PASSWORD" ODP="$ODP" docker-compose up -d --build
 cd ..
