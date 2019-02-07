9ac32e47fcab3b858e641d7e2317f6227d84a7d8
trung <trung@trungs-MacBook-Pro.local>
2019-02-06 22:30:38 -0500

    update backend

--- Backend/gateway-backend.sh
+++ Backend/gateway-backend.sh
@@ -11,30 +11,74 @@ serverip=$(grep MainServerIP | cut -d'=' -f2)
 greyfiship=$(grep GreyFishIp | cut -d'=' -f2)
 
 currentdir=$(pwd)
-# check to see if there are update in folder_to_check for compile job
+# Preparing control files for new execution
 if (ls $control_files_folder/current_compile_file_list > /dev/null 2> /dev/null ); then
 	cp $control_files_folder/current_compile_file_list $control_files_folder/previous_compile_file_list  
 else
 	touch $control_files_folder/previous_compile_file_list 
-fi 
-ls -1r $folder_to_check/*compile.zip > $control_files_folder/current_compile_file_list
+fi
+if (ls $control_files_folder/current_run_file_list > /dev/null 2> /dev/null ); then
+	cp $control_files_folder/current_run_file_list $control_files_folder/previous_run_file_list  
+else
+	touch $control_files_folder/previous_run_file_list 
+fi
 
-new_jobs=$(diff $control_files_folder/current_compile_file_list $control_files_folder/previous_compile_file_list  \
+if (ls $control_files_folder/wait_to_run_file_list > /dev/null 2> /dev/null); then
+	for f in $(cat wait_to_run_file_list); do
+		sed -i "/$f/ d" $control_files_folder/previous_run_file_list
+	done
+fi
+rm $control_files_folder/wait_to_run_file_list 2> /dev/null
+
+
+if (ls $control_files_folder/current_file_list > /dev/null 2> /dev/null ); then
+	cp $control_files_folder/current_file_list $control_files_folder/previous_file_list  
+else
+	touch $control_files_folder/previous_file_list 
+fi 
+ls -1r $folder_to_check/*.zip > $control_files_folder/current_file_list
+
+# check to see if there are update in folder_to_check 
+new_jobs=$(diff $control_files_folder/current_file_list $control_files_folder/previous_file_list  \
 				| grep "^<" | cut -d" " -f2 )
 
-# run all the new compile jobs 
-for j in $new_jobs; do
-	# unzip the compile zip file
+#classify new jobs to Run Job or Compile job
+for j in new_jobs; do
 	name=$(echo $j | rev | cut -d'/' -f1 | rev | cut -d'.' -f1  )
 	unzip $j -d $unzip_files_folder/$name
+	job=$(python3 json_parser.py unzip_files_folder/$name/meta.json Jobs)
+	if [[ $jobs == 'Compile' ]]; then
+		echo $name >> $control_files_folder/current_compile_file_list
+	elif [[ $jobs == 'Run' ]]; then
+		echo $name >> $control_files_folder/current_run_file_list
+	else
+		echo $name >> $control_files_folder/current_compile_file_list
+		echo $name >> $control_files_folder/current_run_file_list
+	fi
+done
+
+
+new_compile_jobs=$(diff $control_files_folder/current_compile_file_list $control_files_folder/previous_compile_file_list  \
+				| grep "^<" | cut -d" " -f2 )
+# run all the new compile jobs 
+for j in $new_compile_jobs; do
+	# unzip the compile zip file
+	name=$j
 	# executing the compile file
 	cd $unzip_files_folder/$name
+	numbersOfCommands=$(python3 json_parser.py meta.json CC)
+	(( numbersOfCommands-- ))
+	touch compile.sh
+	chmod 755 compile.sh
+	for i in $(seq 0 $numbersOfCommands); do
+		python3 json_parser.py meta.json C{$i} >> compile.sh
+	done
 	./compile.sh 2> ErrorMessages.out 
 	# sending back results
 	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
-	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://149.165.156.207:2000/grey/upload/dev/akn752/${name}
+	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
 	zip -r ${name}_output.zip  $unzip_files_folder/$name
-	curl -F file=@$unzip_files_folder/$name/${name}_output.zip http://129.114.17.116:2000/grey/upload/dev/commonuser/output
+	curl -F file=@$unzip_files_folder/$name/${name}_output.zip http://${greyfiship}:2000/grey/upload/dev/commonuser/output
 	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
 	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
 	
@@ -52,55 +96,57 @@ else
 	touch $control_files_folder/previous_running_jobs
 fi
 $show_all_job_ids_command > $control_files_folder/current_running_jobs 
+
+#return results to clients for finished jobs
+finished_jobs=$(diff $control_files_folder/current_running_jobs $control_files_folder/previous_running_jobs\
+				| grep "^>" | cut -d" " -f2)
+for j in $finished_jobs; do
+	name=$j
+	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
+	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
+	curl --header "Content-Type: application/json" --request POST --data "{\"Job_ID\":\"$name\", \"password\":\"abc123\",\"User\":\"akn752\", \"OUTPUT_DIRS\":[], \"OUTPUT_FILES\":[]}" http://${serverip}:5000/listener/api/users/output_data
+	zip -r $unzip_files_folder/$name/${name}_output.zip $unzip_files_folder/$name
+	curl -F file=@$unzip_files_folder/$name/${name}_output.zip http://${greyfiship}:2000/grey/upload/dev/commonuser/output
+	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
+	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
+	curl --header "Content-Type: application/json" --request POST --data "{\"Job_ID\":\"$name\", \"password\":\"abc123\",\"User\":\"akn752\", \"OUTPUT_DIRS\":[], \"OUTPUT_FILES\":[]}" http://${serverip}:5000/listener/api/users/output_data
+done
+
+
 current_running_jobs_num=$(cat $control_files_folder/current_running_jobs  | wc -l)
 num_jobs_to_run=0
 
 if [[ $current_running_jobs_num < $MAXIMUM_RUNNING_JOBS ]]; then
-	#return results to clients for finished jobs
-	finished_jobs=$(diff $control_files_folder/current_running_jobs $control_files_folder/previous_running_jobs\
-					| grep "^>" | cut -d" " -f2)
-	for j in $finished_jobs; do
-		name=$(echo $j | rev | cut -d'/' -f1 | rev | cut -d'.' -f1  )
-		tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
-		curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://149.165.156.207:2000/grey/upload/dev/akn752/${name}
-		curl --header "Content-Type: application/json" --request POST --data "{\"Job_ID\":\"$name\", \"password\":\"abc123\",\"User\":\"akn752\", \"OUTPUT_DIRS\":[], \"OUTPUT_FILES\":[]}" http://149.165.156.207:5000/listener/api/users/output_data
-		zip -r $unzip_files_folder/$name/${name}_output.zip $unzip_files_folder/$name
-		curl -F file=@$unzip_files_folder/$name/${name}_output.zip http://129.114.17.116:2000/grey/upload/dev/commonuser/output
-		tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
-		curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
-		curl --header "Content-Type: application/json" --request POST --data "{\"Job_ID\":\"$name\", \"password\":\"abc123\",\"User\":\"akn752\", \"OUTPUT_DIRS\":[], \"OUTPUT_FILES\":[]}" http://${serverip}:5000/listener/api/users/output_data
-	done
 	#calucalte maximum number of new jobs to run
 	num_jobs_to_run=$(( $MAXIMUM_RUNNING_JOBS - $current_running_jobs_num ))
 fi
-# check to see if there are update in folder_to_check for run job
-if (ls $control_files_folder/current_run_file_list > /dev/null 2> /dev/null ); then
-	cp $control_files_folder/current_run_file_list $control_files_folder/previous_run_file_list   
-else
-	touch $control_files_folder/previous_run_file_list
-fi
 
-ls -1r $folder_to_check/*run.zip > $control_files_folder/current_run_file_list
 
-new_jobs=$(diff $control_files_folder/current_run_file_list $control_files_folder/previous_run_file_list  \
+new_run_jobs=$(diff $control_files_folder/current_run_file_list $control_files_folder/previous_run_file_list  \
 				| grep "^<" | cut -d" " -f2 )
 # run all the new run jobs
-for j in $new_jobs; do
+for j in $new_run_jobs; do
 	# check to see if the threshold of run jobs is reached
 	# if so, does not perform any check or run for new run jobs
 	if [[ $num_jobs_to_run == 0 ]]; then
-		# removing the jobs that cannnot be run this time in the current run file list
-		sed -i "/$j/ d" $control_files_folder/current_run_file_list
+		# saving the jobs that cannnot be run this time in the current run file list
+		echo $j >> $control_files_folder/wait_to_run_file_list
 		continue
 	fi
 
 	# unzip the run zip file
-	name=$(echo $j | rev | cut -d'/' -f1 | rev | cut -d'.' -f1  )
-	unzip $j -d $unzip_files_folder/$name
+	name=$j
 	# run all the new run jobs (FIFO)
 	cd $unzip_files_folder/$name
 	if [[ $scheduling_system == "SLURM" ]]; then
 		cp $currentdir/slurm_skeleton.sh $unzip_files_folder/$name/${name}_slurm_skeleton.sh
+		numbersOfCommands=$(python3 json_parser.py meta.json RC)
+		(( numbersOfCommands-- ))
+		touch run.sh
+		chmod 755 run.sh
+		for i in $(seq 0 $numbersOfCommands); do
+			python3 json_parser.py meta.json R{$i} >> run.sh
+		done
 		cat run.sh >> ${name}_slurm_skeleton.sh
 		sed -i "s/JOB_NAME/$name/g" ${name}_slurm_skeleton.sh
 		sbatch ${name}_slurm_skeleton.sh
