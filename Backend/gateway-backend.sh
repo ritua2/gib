566902bc149f0194bb5ca76b2e6b8a9626769642
trung <trung@trungs-MacBook-Pro.local>
2019-05-29 04:14:29 -0300

    make backend to work for simple testcase on comet

--- Backend/gateway-backend.sh
+++ Backend/gateway-backend.sh
@@ -7,10 +7,20 @@ show_all_job_ids_command=$(grep ShowAllJobIdsCommand configure | cut -d"=" -f2)
 control_files_folder=$(grep ControlFilesFolder configure | cut -d"=" -f2)
 unzip_files_folder=$(grep UnzipFilesFolder configure | cut -d"=" -f2) 
 scheduling_system=$(grep ScheduleingSystem configure | cut -d"=" -f2)
-serverip=$(grep MainServerIP | cut -d'=' -f2)
-greyfiship=$(grep GreyFishIp | cut -d'=' -f2)
+serverip=$(grep MainServerIP configure | cut -d'=' -f2)
+greyfiship=$(grep GreyFishIP configure | cut -d'=' -f2)
+
+
 
 currentdir=$(pwd)
+# Getting the files from greyfish
+cd $folder_to_check
+curl -O http://$greyfiship:2000/grey/grey_dir/greyfish/commonuser/jobs_left
+tar -xzf jobs_left
+rm jobs_left
+
+cd $currentdir
+
 # Preparing control files for new execution
 if (ls $control_files_folder/current_compile_file_list > /dev/null 2> /dev/null ); then
 	cp $control_files_folder/current_compile_file_list $control_files_folder/previous_compile_file_list  
@@ -43,10 +53,10 @@ new_jobs=$(diff $control_files_folder/current_file_list $control_files_folder/pr
 				| grep "^<" | cut -d" " -f2 )
 
 #classify new jobs to Run Job or Compile job
-for j in new_jobs; do
+for j in $new_jobs; do
 	name=$(echo $j | rev | cut -d'/' -f1 | rev | cut -d'.' -f1  )
-	unzip $j -d $unzip_files_folder/$name
-	job=$(python3 json_parser.py unzip_files_folder/$name/meta.json Jobs)
+	unzip $j -d $unzip_files_folder #/$name
+	job=$(python json_parser.py unzip_files_folder/$name/meta.json Jobs)
 	if [[ $jobs == 'Compile' ]]; then
 		echo $name >> $control_files_folder/current_compile_file_list
 	elif [[ $jobs == 'Run' ]]; then
@@ -65,28 +75,29 @@ for j in $new_compile_jobs; do
 	# unzip the compile zip file
 	name=$j
 	# executing the compile file
+	echo $currentdir
 	cd $unzip_files_folder/$name
-	numbersOfCommands=$(python3 json_parser.py meta.json CC)
-	(( numbersOfCommands-- ))
+	numbersOfCommands=$(python ${currentdir}/json_parser.py meta.json CC)
+	numbersOfCommands=$((numbersOfCommands - 1))
+	username=$(python ${currentdir}/json_parser.py meta.json User)
+	
 	touch compile.sh
 	chmod 755 compile.sh
 	for i in $(seq 0 $numbersOfCommands); do
-		python3 json_parser.py meta.json C{$i} >> compile.sh
+		python ${currentdir}/json_parser.py meta.json C${i} >> compile.sh
 	done
 	./compile.sh 2> ErrorMessages.out 
 	# sending back results
-	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
-	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
-	zip -r ${name}_output.zip  $unzip_files_folder/$name
-	curl -F file=@$unzip_files_folder/$name/${name}_output.zip http://${greyfiship}:2000/grey/upload/dev/commonuser/output
-	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
-	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
-	
 	cd $currentdir
+	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
+	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/greyfish/${username}/${name}
+
+	# zip -r ${name}_output.zip  $unzip_files_folder/$name
+	# curl -F file=@$unzip_files_folder/$name/${name}_output.zip http://${greyfiship}:2000/grey/upload/dev/commonuser/output
+
+	# tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
+	# curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
 done
-
-
-
 
 MAXIMUM_RUNNING_JOBS=50
 
@@ -100,16 +111,20 @@ $show_all_job_ids_command > $control_files_folder/current_running_jobs
 #return results to clients for finished jobs
 finished_jobs=$(diff $control_files_folder/current_running_jobs $control_files_folder/previous_running_jobs\
 				| grep "^>" | cut -d" " -f2)
+echo $finished_jobs
 for j in $finished_jobs; do
-	name=$j
+	name=$(grep $j $control_files_folder/job_id_and_name_list | cut -d' '  -f2)
+	cd $unzip_files_folder/$name
+	username=$(python ${currentdir}/json_parser.py meta.json User)
+	cd $currentdir
 	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
-	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
-	curl --header "Content-Type: application/json" --request POST --data "{\"Job_ID\":\"$name\", \"password\":\"abc123\",\"User\":\"akn752\", \"OUTPUT_DIRS\":[], \"OUTPUT_FILES\":[]}" http://${serverip}:5000/listener/api/users/output_data
-	zip -r $unzip_files_folder/$name/${name}_output.zip $unzip_files_folder/$name
-	curl -F file=@$unzip_files_folder/$name/${name}_output.zip http://${greyfiship}:2000/grey/upload/dev/commonuser/output
-	tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
-	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
-	curl --header "Content-Type: application/json" --request POST --data "{\"Job_ID\":\"$name\", \"password\":\"abc123\",\"User\":\"akn752\", \"OUTPUT_DIRS\":[], \"OUTPUT_FILES\":[]}" http://${serverip}:5000/listener/api/users/output_data
+	curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/greyfish/${username}/${name}
+	# curl --header "Content-Type: application/json" --request POST --data "{\"Job_ID\":\"$name\", \"password\":\"abc123\",\"User\":\"akn752\", \"OUTPUT_DIRS\":[], \"OUTPUT_FILES\":[]}" http://${serverip}:5000/listener/api/users/output_data
+	# zip -r $unzip_files_folder/$name/${name}_output.zip $unzip_files_folder/$name
+	# curl -F file=@$unzip_files_folder/$name/${name}_output.zip http://${greyfiship}:2000/grey/upload/dev/commonuser/output
+	# tar -czf $unzip_files_folder/${name}_output.tar.gz $unzip_files_folder/$name/*
+	# curl -F file=@$unzip_files_folder/${name}_output.tar.gz http://${greyfiship}:2000/grey/upload/dev/akn752/${name}
+	# curl --header "Content-Type: application/json" --request POST --data "{\"Job_ID\":\"$name\", \"password\":\"abc123\",\"User\":\"akn752\", \"OUTPUT_DIRS\":[], \"OUTPUT_FILES\":[]}" http://${serverip}:5000/listener/api/users/output_data
 done
 
 
@@ -137,24 +152,25 @@ for j in $new_run_jobs; do
 	# unzip the run zip file
 	name=$j
 	# run all the new run jobs (FIFO)
-	cd $unzip_files_folder/$name
 	if [[ $scheduling_system == "SLURM" ]]; then
 		cp $currentdir/slurm_skeleton.sh $unzip_files_folder/$name/${name}_slurm_skeleton.sh
-		numbersOfCommands=$(python3 json_parser.py meta.json RC)
-		(( numbersOfCommands-- ))
+		cd $unzip_files_folder/$name
+		numbersOfCommands=$(python ${currentdir}/json_parser.py meta.json RC)
+		numbersOfCommands=$((numbersOfCommands - 1))
 		touch run.sh
 		chmod 755 run.sh
 		for i in $(seq 0 $numbersOfCommands); do
-			python3 json_parser.py meta.json R{$i} >> run.sh
+			python ${currentdir}/json_parser.py meta.json R${i} >> run.sh
 		done
 		cat run.sh >> ${name}_slurm_skeleton.sh
 		sed -i "s/JOB_NAME/$name/g" ${name}_slurm_skeleton.sh
-		sbatch ${name}_slurm_skeleton.sh
+		jobid=$(sbatch ${name}_slurm_skeleton.sh | cut -d' ' -f4)
+		cd $currentdir
 		# adding new job to the list of current_running_jobs
-		echo $name >> $control_files_folder/current_running_jobs
+		echo $jobid >> $control_files_folder/current_running_jobs
+		echo "$jobid $name" >> $control_files_folder/job_id_and_name_list
 	fi
-	cd $currentdir
-	(( num_jobs_to_run-- ))
+	num_jobs_to_run=$((num_jobs_to_run - 1))
 done
 
 
