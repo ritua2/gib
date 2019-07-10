675d6ba8680ca7d2a5c8a43ab2f0c403730f1345
Carlos Redondo <carlos.red@utexas.edu>
2019-07-10 16:39:13 -0500

    Added ssh functionaility to greyfish container to allow for future rsync

--- middle-layer/greyfish_storage/API_Daemon.sh
+++ middle-layer/greyfish_storage/API_Daemon.sh
@@ -15,7 +15,7 @@ if [ $1 == "-h" ]; then
    printf "Use flag -up to set-up the APIs\n"
    printf "Use flag -down to cancel the APIs\n"
    printf "\n All APIs will be started with 4 workers, modify this file if more workers are required\n"
-   exit 1
+   exit 0
 fi
 
 
@@ -25,13 +25,18 @@ if [ $1 == "-up" ]; then
   gunicorn -w $greyfish_threads -b 0.0.0.0:2002 push_all:app &
   gunicorn -w $greyfish_threads -b 0.0.0.0:2003 new_user:app &
   gunicorn -w $greyfish_threads -b 0.0.0.0:2004 admin:app &
+  printf "rc-status:\n"
+  rc-status
+  /etc/init.d/sshd start
   printf "Greyfish APIs are now active\n"
+  exit 0
 fi
 
 
 if [ $1 == "-down" ]; then 
    
    pkill gunicorn
-
+   /etc/init.d/sshd stop
    printf "Greyfish APIs have been disconnected\n"
+   exit 0
 fi
